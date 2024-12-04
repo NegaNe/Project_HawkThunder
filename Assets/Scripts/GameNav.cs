63a436de3afb28618595c13854751c59e3d3e2f9
NegaNe <manfireburst@gmail.com>
2024-12-05 01:47:40 +0700

    finale

--- Assets/Scripts/GameNav.cs
+++ Assets/Scripts/GameNav.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameNav : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
     }
-
-    // Update is called once per frame
-    void Update()
+        public void Retry()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Pause()
+    {
+    
+    Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+    Time.timeScale = 1;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
